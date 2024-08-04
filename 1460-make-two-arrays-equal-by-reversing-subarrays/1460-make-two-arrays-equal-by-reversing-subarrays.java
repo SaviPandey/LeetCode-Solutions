237c8ed13347a018073e701f6b9fe23156a3a6cd
Savinay Pandey <140955852+SaviPandey@users.noreply.github.com>
2024-08-04 05:42:17 +0530

    Time: 1 ms (100.00%), Space: 43.9 MB (92.78%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,12 +1,30 @@
 class Solution {
     public boolean canBeEqual(int[] a, int[] b) {
-    Map<Integer, Integer> map = new HashMap<>();
-    for (int i = 0; i < a.length; i++) {
-        map.put(a[i], map.getOrDefault(a[i], 0) + 1);
-        map.put(b[i], map.getOrDefault(b[i], 0) - 1);
+
+    // 1) Map Approach :
+    // Map<Integer, Integer> map = new HashMap<>();
+    // for (int i = 0; i < a.length; i++) {
+    //     map.put(a[i], map.getOrDefault(a[i], 0) + 1);
+    //     map.put(b[i], map.getOrDefault(b[i], 0) - 1);
+    // }
+    // for (int i : map.keySet()) {
+    //     if (map.get(i) != 0) return false;
+    // }
+    // return true;
+
+    if(a.length != b.length) {
+        return false;
     }
-    for (int i : map.keySet()) {
-        if (map.get(i) != 0) return false;
+    int elementsCount[] = new int[1001];
+
+    for(int i=0; i < a.length; i++) {
+        elementsCount[a[i]]++;
+        elementsCount[b[i]]--;
+    }
+
+    for(int count : elementsCount) {
+        if(count != 0) 
+            return false;
     }
     return true;
 }
