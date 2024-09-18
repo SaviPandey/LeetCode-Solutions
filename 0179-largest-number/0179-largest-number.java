e8f1c199f6932545a06c2e5f62315ba32392f28a
Savinay Pandey <140955852+SaviPandey@users.noreply.github.com>
2024-09-18 15:01:32 +0530

    Time: 5 ms (98.24%), Space: 43.1 MB (55.89%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -6,20 +6,25 @@ class Solution {
        for(int i=0; i<nums.length; i++){
         str[i] = nums[i]+"";
        } 
-       Arrays.sort(str, new Comparator<String>() {
-        @Override
-        public int compare(String i, String j) {
-            String s1 = i + j;
-            String s2 = j + i;
-            return s1.compareTo(s2);
-        }
-       });
+    //    Arrays.sort(str, new Comparator<String>() {
+    //     @Override
+    //     public int compare(String i, String j) {
+    //         String s1 = i + j;
+    //         String s2 = j + i;
+    //         return s2.compareTo(s1);
+    //     }
+    //    });
+        Arrays.sort(str, (i, j) -> (j + i).compareTo(i + j)); // Optimized comparator with lambda
 
-       if(str[str.length - 1].charAt(0) == '0') return "0";  //Not Printing 00, 000..
-       String res = new String();
-       for(int i = 0; i < str.length; i++){
-        res = str[i] + res;
-       }
-       return res;
+        if (str[0].equals("0")) return "0";
+
+    //    if(str[str.length - 1].charAt(0) == '0') return "0";  //Not Printing 00, 000..
+
+        StringBuilder res = new StringBuilder();
+        for (String s : str) {
+            res.append(s);
+        }
+
+        return res.toString();
     }
 }
