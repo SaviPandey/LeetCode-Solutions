4dcecd7825aa55cbaf537c72a8d3dd53d0b8c01d
Savinay Pandey <140955852+SaviPandey@users.noreply.github.com>
2024-10-25 02:05:16 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (8.42%) - LeetHub

---
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+        public boolean flipEquiv(TreeNode r1, TreeNode r2) {
+        if (r1 == null || r2 == null) return r1 == r2;
+        return r1.val == r2.val &&
+               (flipEquiv(r1.left, r2.left) && flipEquiv(r1.right, r2.right) || 
+               flipEquiv(r1.left, r2.right) && flipEquiv(r1.right, r2.left));
+    }
+}
