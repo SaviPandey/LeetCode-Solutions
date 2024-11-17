7494675f46e7e02d739db5badc587aeb04ff9530
Savinay Pandey <140955852+SaviPandey@users.noreply.github.com>
2024-11-17 17:28:23 +0530

    Time: 38 ms (51.57%), Space: 62.7 MB (19.49%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.java
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.java
@@ -1,32 +1,66 @@
-class Pair {
-    long sumValue;
-    int index;
-    Pair(long sumValue, int index) {
-        this.sumValue = sumValue;
-        this.index = index;
-    }
-}
+// class Pair {
+//     long sumValue;
+//     int index;
+//     Pair(long sumValue, int index) {
+//         this.sumValue = sumValue;
+//         this.index = index;
+//     }
+// }
+
+// 1st-Approach (MinHeap) 
+// class Solution {
+//     public int shortestSubarray(int[] nums, int k) {
+//         int n = nums.length;
+//         int minAns = Integer.MAX_VALUE;
+//         long prefixSum = 0;
+
+//         PriorityQueue<Pair> prefixSumHeap = new PriorityQueue<>((a, b) -> Long.compare(a.sumValue , b.sumValue));
+
+//         for(int j = 0; j < n ; j++) {
+//             prefixSum += nums[j];
+
+//             if(prefixSum >= k)
+//                 minAns = Math.min(minAns, j + 1);
+            
+//             while(!prefixSumHeap.isEmpty() && prefixSum - prefixSumHeap.peek().sumValue >= k){
+//                 minAns = Math.min(minAns, j - prefixSumHeap.poll().index);
+//             }
+
+//             prefixSumHeap.offer(new Pair(prefixSum, j));
+//         }
+
+//         return minAns == Integer.MAX_VALUE ? -1 : minAns;
+    
+//     }
+// }
+
+// 2nd Approach - (Monotonic Deque)
 class Solution {
     public int shortestSubarray(int[] nums, int k) {
-        int n = nums.length;
-        int minAns = Integer.MAX_VALUE;
-        long prefixSum = 0;
+       int n = nums.length;
+       long prefixSums[] = new long[n + 1]; 
 
-        PriorityQueue<Pair> prefixSumHeap = new PriorityQueue<>((a, b) -> Long.compare(a.sumValue , b.sumValue));
+       for(int j = 0; j < n; j++) {
+         prefixSums[j] = j > 0 ? prefixSums[j - 1] + nums[j] : nums[j];
+       }
 
-        for(int j = 0; j < n ; j++) {
-            prefixSum += nums[j];
+       Deque<Integer> dq = new ArrayDeque<>(); 
+       int minAns = Integer.MAX_VALUE;
 
-            if(prefixSum >= k)
+       for(int j = 0; j < n; j++) {
+            if(prefixSums[j] >= k) 
                 minAns = Math.min(minAns, j + 1);
-            
-            while(!prefixSumHeap.isEmpty() && prefixSum - prefixSumHeap.peek().sumValue >= k){
-                minAns = Math.min(minAns, j - prefixSumHeap.poll().index);
+
+            while(!dq.isEmpty() && prefixSums[j] - prefixSums[dq.peekLast()] >= k) {
+                minAns = Math.min(minAns, j - dq.pollLast());
             }
 
-            prefixSumHeap.offer(new Pair(prefixSum, j));
-        }
+            while(!dq.isEmpty() && prefixSums[j] <= prefixSums[dq.peekFirst()]) {
+                dq.pollFirst();
+            }
 
+            dq.addFirst(j);
+       }
         return minAns == Integer.MAX_VALUE ? -1 : minAns;
     
     }
